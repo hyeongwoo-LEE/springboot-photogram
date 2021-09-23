8ed971d54386d7ddeffe30d89324174383c05938
hyeongwoo-LEE <lhw970804@naver.com>
2021-09-23 19:41:07 +0900

    82강

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.config;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +11,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.awt.image.BandCombineOp;
 
+@RequiredArgsConstructor
 @EnableWebSecurity //해당 파일로 시큐리티를 활성화
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2DetailsService oAuth2DetailsService;
 
     @Bean
     public BCryptPasswordEncoder encoder(){
@@ -28,6 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/auth/signin") //get
                 .loginProcessingUrl("/auth/signin") //post
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login() //form 로그인도 하는데, oauth2 로그인도 할거야
+                .userInfoEndpoint() //oauth2 로그인을 하면 최종응답을 회원정보를 바로 받을 수 있다. (중간과정 건너뜀)
+                .userService(oAuth2DetailsService);
     }
 }
